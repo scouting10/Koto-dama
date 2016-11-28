3e6fb12107b133c192119ce64fa925b256ccf721
Dai Watanabe <scouting10@hotmail.com>
2016-11-28 17:16:13 +0900

    Fix: DeckController, HandController
    HandControllerにて、OnMouseDragを実装しようとするも失敗。その他、合成後のDeckControllerとのやりとりについても記載している。

--- Assets/App/Game/Scripts/DeckController.cs
+++ Assets/App/Game/Scripts/DeckController.cs
@@ -10,14 +10,112 @@ public class DeckController : MonoBehaviour
 	[SerializeField]
 	private HandController _handController;
 
+
+	// Prefabの設定
+	public GameObject deckCardPrefab;
+	public GameObject handCardPrefab;
+
+	/// masterdatamanagerのスクリプトをここに埋め込む？
+	/// public MasterDataManager _masterDataManager;
+	/// 
+
+	// HandCardの位置
+	private float handPos_x;
+	private float handPos_z = 3f;
+
+
+	// DeckCardの位置
+	private float deckPos_x = -5f;
+	private float deckPos_z = -3f;
+
+
+
 	/// <summary>
 	/// Initialize this instance.
 	/// </summary>
 	public void Initialize ()
 	{
-		//手札の初期化
+		
+
+		// HandCard配置
+		for (int i = -5; i < -2; i++) {
+			GameObject firstDraw = Instantiate (handCardPrefab)as GameObject;
+			// ↓多分ここにランダムにmasterdatamanagerからの
+
+			// ↑カード情報をもらうコードが必要
+			firstDraw.transform.position = new Vector3 (i, 0, handPos_z);
+		}
+
+		// DeckCard配置
+		GameObject deckSet = Instantiate (deckCardPrefab)as GameObject;
+		deckSet.transform.position = new Vector3 (deckPos_x, 0, deckPos_z);
+
+		//comCardIdの初期化
+		//comCardId=ComCard(string a,string b);
+
+		Debug.Log (MasterDataManager.Instance.wordMasterData._wordRawDataList[0].id);
+
+
+		// 手札の初期化　　　↓　これって、それぞれのPrefabできちんと仕事すんの？
+		// 下で新しいカードを生み出すたびに、Initialize()は必要。現在未実装
 		_handController.Initialize ();	
 
 
+
 	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		// HandCardが減ったのを検出して、カードを送る。
+		// ↑これいらんかもなぁ、、、カードが消えるタイミングは、全て把握できるはずで、HandControllerから送られるかも。
+
+
+	}	
+
+	// HandControllerから呼ばれる、手札合成時にカード作る関数
+	public void ComCard (string a, string b, Vector3 c){
+		GameObject newCard = Instantiate (handCardPrefab) as GameObject;
+		newCard.transform.position = c;
+		Initialize ();
+		// "a+b+の組み合わせから、MasterDataManagerから合成後のId引っ張ってくる。";
+		// newCard.GetComponent<"HandController">().id = ???
+	}
+
+	// HandControllerから呼ばれる、Deckアタッチ時にカード作る関数
+	// Deckにアタッチした時に起きること、の記述はdeckcontroller内にすると思うので、引数にはカードidも渡す。
+	public void ComDeck (string a, Vector3 c){
+		GameObject newCard = Instantiate (handCardPrefab) as GameObject;
+		newCard.transform.position = c;
+		// Randomに、MasterDataManagerから合成後のId引っ張ってくる。
+			// ↑これも微妙。アタッチするカードの効果による。
+		// newCard.GetComponent<"HandController">().id = Randomなんちゃら?
+
+	}
+
+
+	// HandControllerから呼ばれる、フィールドアタッチ、オブジェクトアタッチ時にカード作る関数
+	// おそらくだが、ComCardと異なり、カードid送り先はフィールド側、オブジェクト側の関数内になるはず
+	// なので、ComField()とComObject（）は初期位置しか引数に持たない（はず）。
+	public void ComField (Vector3 c){
+		GameObject newCard = Instantiate (handCardPrefab) as GameObject;
+
+		newCard.transform.position = c;
+		// Randomに、MasterDataManagerから合成後のId引っ張ってくる。;
+		// newCard.GetComponent<"HandController">().id = ???
+
+	}
+	public void ComObject (Vector3 c){
+		GameObject newCard = Instantiate (handCardPrefab) as GameObject;
+		newCard.transform.position = c;
+		// Randomに、MasterDataManagerから合成後のId引っ張ってくる。;
+		// newCard.GetComponent<"HandController">().id = ???
+
+	}
+
+
+
 }
--- Assets/App/Game/Scripts/HandController.cs
+++ Assets/App/Game/Scripts/HandController.cs
@@ -9,80 +9,176 @@ public class HandController : MonoBehaviour
 {
 	[SerializeField]
 	private	DeckController _deckController;
+		// ↑んー？これ今のままじゃ使えんな。
 
-	//カード離した先での判定Bool
+	// Deckcontrollerからもらうカードデータ
+	public string id;
+		// など、など
+
+	// 元に戻る用、次の配置用の、カード初期位置の保存
+	private Vector3 startPos = new Vector3();
+
+
+
+	// OnTriggerでの判定
 	private bool withCard = false;
 	private bool withField = false;
 	private bool withDeck = false;
 	private bool withObject = false;
 
-	//カード合成時、OnTriggerからUpdateに渡される、「相手カード」のid
+	// カード合成時
+		// OnTriggerからUpdateに渡される、「相手カード」のid
 	private string handCardId_b;
+		//DeckControllerの合成関数ComCard()を呼ぶためのDeckController変数
+	private DeckController deckController;
+		/*↑こんなんせんでも、せっかくSerializeで設定した変数があった。
+			↑現状使えない。高取先生に質問。*/
+
 
 	/// <summary>
 	/// Initialize this instance.
 	/// </summary>
 	public void Initialize ()
 	{
-		
+		//初期データを格納
+		startPos = this.transform.position;
+		//id = もらえるカードid
+
+		//カードアタッチ時（to カード、フィールド、オブジェクト）に使用
+		deckController = GameObject.Find ("DeckController").GetComponent<DeckController>();
+			/*↑こんなんせんでも、Serializeで_deckControllerが設定されてる。
+			_deckController使った瞬間、下のComCard()とかの赤字が一瞬で黒字化した。
+				↑現状使えない。高取先生に質問。*/
 
 		
 	}
 
 
-	/*void Update() 
+	void Update() 
 	{
-		//指でつままれたら、指の位置に座標を変える。
-		if (Input.GetkeyStay (0) && tag == "HandCard") {
-			this.transfrom.position = new Vector3 (指のいちx, 0, 指のいちｚ);
-		}	
-		
-		// んで、指を離した時、その座標がカードと重なってたら、合成関数を走らせる。
-		// カードと重なってるか否かは、下記のOnTriggerでwithCatdとして検出。
-		if (Input.GetKeyUp (0) && withCard ==true) {
-			string handCardId_a = this.id;
-			CardGenerator内の合成関数（handCardId_a,handardId_b）;
-			// で、多分ここでDestroy? 相手カードってどうやって壊すん？？
-			Destroy.this;
+		// ↓というわけで、Onmousedragを試してみる。OnMouseDrag()は、Updateの外で記述。
+
+		//指でつままれたら、指の位置に座標を変える。　ここ相当適当に書いてるから、これから直す。
+		/*
+		if (Input.GetMouseButton (0)) {
+			// クリック中の座標を取得。y座標は邪魔なのでカット。しかし、カメラ傾けてたら機能しなさそう、、、
+	        Vector3 mousePos_screen = Input.mousePosition;
+			Vector3 mousePos_world = Camera.main.ScreenToWorldPoint (mousePos_screen);
+			mousePos = new Vector3(mousePos_world.x, 0,mousePos_world.z);
+
+			// ↑というわけで、Rayを使ってみる。が、オブジェクト動かず。
+			
+			Ray ray= Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			//if (hit.collider == this) { このifがあると、コンパイルエラーになる。
+				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+					float x = hit.point.x;
+					float z = hit.point.z;
+					this.transform.position = new Vector3 (x, 0.0f, z);
+					Debug.Log ("Hit");
+				}
+
+			//}
+		} */
+
+
+		// んで、指を離した時、その座標が「何か」と重なってたら、合成関数を走らせる。
+		// 何かと重なってるか否かは、下記のOnTriggerEnter,OnTriggerExitでwith「何か」として検出。
+		if (Input.GetMouseButtonUp (0)) {
+			if (withCard) {
+				string handCardId_a = this.id;
+				deckController.ComCard(handCardId_a, handCardId_b, startPos);
+				Debug.Log ("withCard");
+				// withなんたらを初期化する。
+				withCard = false;		
+				Destroy (this.gameObject);
+
+			} else if (withDeck) {
+				string handCardId_a = this.id;
+				deckController.ComDeck(handCardId_a, startPos);
+				withDeck = false;		
+				Destroy (this.gameObject);
+
+			} else if (withField) {
+				deckController.ComField(startPos);
+				withField = false;		
+				Destroy (this.gameObject);
+
+			} else if (withObject) {
+				deckController.ComObject(startPos);
+				withObject = false;		
+				Destroy (this.gameObject);
+			
+			} else {
+				// カードを初期位置へ
+				// 本当は、動いて戻したい。↓だと、パッと瞬間移動する。
+				// 多分、 speedを定義して、毎フレームそれだけ戻るようにするのだが、、、？
+				this.transform.position = startPos;
+			}
 		}
+	}
+
+	// ↓保留。最終的にこの形で同じ（バグった）挙動する。
+	void OnMouseDrag(){
+		/*
+		Vector3 objectPointInScreen
+		= Camera.main.WorldToScreenPoint(this.transform.position);
+		*/
+		// mousePositionは、Screen座標系
+		Vector3 mousePointInScreen = Input.mousePosition;
+		/* ↑のように、まとめてはいけないのか？
+		= new Vector3(Input.mousePosition.x,
+			// この処理で、動く予定のないy座標はobjectとmouseで合わせてしまう。
+			objectPointInScreen.y,
+			Input.mousePosition.z);
+		*/
+
+		// y座標の処理を終えたmousePointInScreenを、World座標に戻し、objectのtransform(World座標）に代入。
+		Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+		mousePointInWorld.y = this.transform.position.y;
+		this.transform.position = mousePointInWorld;
+	
+
 	}
 		
 
 	
-
+	/*
 	void OnTriggerEnter(Collider other){
 		// ドラッグを離した時、HandCardと重なっていたら、
 		// withCard = trueを返す。それによって、Update()からcardgenerator内の合成関数走らせる。
-		if(other.tag == HandCard){
-			withCard = true;
-			//Update()で相手のidを使う為に格納する。
-			handCardId_b = this.id;
+		if (other.gameObject.tag == "HandCard") {
+				withCard = true;
+			// Update()内のComCard()の引数。相手カードのidを取得する。
+				handCardId_b = this.id;
+				Destroy(other.gameObject);
 
 		//その他の条件も記述
-		}else if(other.tag == Field){
+		} else if (other.gameObject.tag == "Field") {
 			withField = true;
-		}else if(other.tag == Deck){
+		} else if (other.gameObject.tag == "Deck") {
 			withDeck = true;
-		}else if(other.tag == Object){
+		} else if (other.gameObject.tag == "Obj") {
 			withObject = true;
 		}
+	}
 
 	//多分、OnTriggerExitで一つずつfalseに直すコードも必要。
 	void OnTriggerExit(Collider other){
 		// 対象から外れた時、withなんたらをfalseに戻す。
-		if(other.tag==HandCard){
+		if(other.gameObject.tag=="HandCard"){
 			withCard = false;
 		//その他の条件も記述
-		}else if(other.tag == Field){
+		}else if(other.gameObject.tag == "Field"){
 			withField = false;
-		}else if(other.tag == Deck){
+		}else if(other.gameObject.tag == "Deck"){
 			withDeck = false;
-		}else if(other.tag == Object){
-			withObject = true;
+		}else if(other.gameObject.tag == "Obj"){
+			withObject = false;
 		}
 
 
 
-	}*/
-	
+	}
+	*/
 }
